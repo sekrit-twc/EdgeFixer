0a0f5b7c06014cadab5b0e839c02c96e41cbaadd
sekrit-twc <noreply@example.com>
2017-01-15 17:11:00 -0800

    Cosmetics.

--- EdgeFixer/edgefixer.c
+++ EdgeFixer/edgefixer.c
@@ -13,14 +13,14 @@ typedef struct least_squares_data {
 
 static void least_squares(int n, least_squares_data *d, float *a, float *b)
 {
-	float interval_x = (float) (d[n - 1].integral_x - d[0].integral_x);
-	float interval_y = (float) (d[n - 1].integral_y - d[0].integral_y);
-	float interval_xy = (float) (d[n - 1].integral_xy - d[0].integral_xy);
-	float interval_xsqr = (float) (d[n - 1].integral_xsqr - d[0].integral_xsqr);
+	float interval_x = (float)(d[n - 1].integral_x - d[0].integral_x);
+	float interval_y = (float)(d[n - 1].integral_y - d[0].integral_y);
+	float interval_xy = (float)(d[n - 1].integral_xy - d[0].integral_xy);
+	float interval_xsqr = (float)(d[n - 1].integral_xsqr - d[0].integral_xsqr);
 
 	/* Add 0.001f to denominator to prevent division by zero. */
-	*a = ((float) n * interval_xy - interval_x * interval_y) / ((interval_xsqr * (float) n - interval_x * interval_x) + 0.001f);
-	*b = (interval_y - *a * interval_x) / (float) n;
+	*a = ((float)n * interval_xy - interval_x * interval_y) / ((interval_xsqr * (float)n - interval_x * interval_x) + 0.001f);
+	*b = (interval_y - *a * interval_x) / (float)n;
 }
 
 static uint8_t float_to_u8(float x)
@@ -35,15 +35,16 @@ size_t edgefixer_required_buffer(int n)
 
 void edgefixer_process_edge(uint8_t *x, const uint8_t *y, int x_dist_to_next, int y_dist_to_next, int n, int radius, void *tmp)
 {
-	least_squares_data *buf = (least_squares_data *) tmp;
+	least_squares_data *buf = (least_squares_data *)tmp;
 	float a, b;
+	int i;
 
 	buf[0].integral_x = x[0];
 	buf[0].integral_y = y[0];
 	buf[0].integral_xy = x[0] * y[0];
 	buf[0].integral_xsqr = x[0] * x[0];
 
-	for (int i = 1; i < n; ++i) {
+	for (i = 1; i < n; ++i) {
 		int _x = x[i * x_dist_to_next];
 		int _y = y[i * y_dist_to_next];
 
@@ -54,7 +55,7 @@ void edgefixer_process_edge(uint8_t *x, const uint8_t *y, int x_dist_to_next, in
 	}
 
 	if (radius) {
-		for (int i = 0; i < n; ++i) {
+		for (i = 0; i < n; ++i) {
 			int left = i - radius;
 			int right = i + radius;
 
@@ -67,127 +68,7 @@ void edgefixer_process_edge(uint8_t *x, const uint8_t *y, int x_dist_to_next, in
 		}
 	} else {
 		least_squares(n, buf, &a, &b);
-		for (int i = 0; i < n; ++i)
+		for (i = 0; i < n; ++i)
 			x[i * x_dist_to_next] = float_to_u8(x[i * x_dist_to_next] * a + b);
 	}
 }
-
-#if 0
-#include <stdio.h>
-
-static int read_frame(pixel_t *buf, FILE *in, int width, int height)
-{
-	for (int i = 0; i < height; ++i) {
-		if (fread(buf + i * width, sizeof(pixel_t), width, in) != width)
-			return -1;
-	}
-	return 0;
-}
-
-static int write_frame(pixel_t *buf, FILE *out, int width, int height)
-{
-	for (int i = 0; i < height; ++i) {
-		if (fwrite(buf + i * width, sizeof(pixel_t), width, out) != width)
-			return -1;
-	}
-	return 0;
-}
-
-static int fix_edges(const char *infile, const char *outfile, int width, int height, int radius)
-{
-	FILE *in = NULL, *out = NULL;
-	pixel_t *frame = NULL;
-	void *tmp = NULL;
-	int ret = -1;
-
-	in = fopen(infile, "rb");
-	if (!in) {
-		fprintf(stderr, "error opening input file: %s\n", infile);
-		goto fail;
-	}
-
-	out = fopen(outfile, "wb");
-	if (!out) {
-		fprintf(stderr, "error opening output file: %s\n", outfile);
-		goto fail;
-	}
-
-	frame = (pixel_t *) malloc(width * height * sizeof(pixel_t));
-	if (!frame) {
-		fprintf(stderr, "error allocating frame\n");
-		goto fail;
-	}
-
-	if (read_frame(frame, in, width, height)) {
-		fprintf(stderr, "error reading frame");
-		goto fail;
-	}
-
-	tmp = malloc(required_buffer(width > height ? width : height));
-	if (!tmp) {
-		fprintf(stderr, "error allocating temporary buffer");
-		goto fail;
-	}
-
-	/* Left */
-	process_edge(&frame[0], &frame[1], width, width, height, radius, tmp);
-
-	/* Right. */
-	process_edge(&frame[width - 1], &frame[width - 2], width, width, height, radius, tmp);
-
-	/* Top. */
-	process_edge(&frame[0], &frame[width], 1, 1, width, radius, tmp);
-
-	/* Bottom. */
-	process_edge(&frame[width * (height - 1)], &frame[width * (height - 2)], 1, 1, width, radius, tmp);
-
-	if (write_frame(frame, out, width, height)) {
-		fprintf(stderr, "error writing frame");
-		goto fail;
-	}
-
-	ret = 0;
-fail:
-	free(tmp);
-	free(frame);
-	if (out)
-		fclose(out);
-	if (in)
-		fclose(in);
-	return ret;
-}
-
-int main(int argc, const char **argv)
-{
-	int width, height, radius;
-	const char *infile, *outfile;
-
-	if (argc != 6) {
-		fprintf(stderr, "Usage: EdgeFixer width height radius infile outfile\n");
-		return -1;
-	}
-
-	width = atoi(argv[1]);
-	if (width == 0) {
-		fprintf(stderr, "error in width\n");
-		return -1;
-	}
-
-	height = atoi(argv[2]);
-	if (height == 0) {
-		fprintf(stderr, "error in height\n");
-		return -1;
-	}
-
-	radius = atoi(argv[3]);
-	infile = argv[4];
-	outfile = argv[5];
-
-	if (fix_edges(infile, outfile, width, height, radius)) {
-		fprintf(stderr, "internal error\n");
-		return -1;
-	}
-
-	return 0;
-}
-#endif
